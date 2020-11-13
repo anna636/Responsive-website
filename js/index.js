da8154655de9b3eee59edfb7d28c50328af5d217
TiredBirb <72065138+anna636@users.noreply.github.com>
2020-11-13 18:47:56 +0100

    Added pictures

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const nav = $('.nav-menu');
+const navigation = $('.navigation');
+const openBtn = $('.hamburger');
+const closeBtn = $('.close');
+
+const navLeft = nav.position().left;
+console.log(navLeft);
+openBtn.on('click', () => {
+  if (navLeft < 0) {
+    navigation.addClass('show');
+    nav.addClass('show');
+    $('body').addClass('show');
+
+  }
+
+});
+
+closeBtn.on('click', () => {
+  if (navLeft < 0) {
+    navigation.removeClass('show');
+    nav.removeClass('show');
+    $('body').removeClass('show');
+
+  }
+
+});
